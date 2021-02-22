49103817f776c60a65a5f0e8d14167e5b69521a4
flauber <flauber@users.noreply.github.com>
2021-02-22 10:24:09 -0300

    Update evolution_jason.mk

--- evolution_jason.mk
+++ evolution_jason.mk
@@ -25,7 +25,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
 PRODUCT_NAME := evolution_jason
 PRODUCT_BRAND := Xiaomi
